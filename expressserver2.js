06ec0bf031864bd25a9c35a7c3545d3c105dd81a
Louise <loni18ac@student.cbs.dk>
2020-10-18 10:26:49 +0200

    s

--- expressserver2.js
+++ expressserver2.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+//Vi importerer express
 const User = require('./User.js')
 const FreeUser = require('./FreeUser.js')
 const PaymentUser = require('./PaymentUser')
@@ -9,24 +9,10 @@ const image = require('./image')
 //alle klasser er nu gemt i constants i serveren
 
 const Server = express()
-//app sættes til at kalde på funtionen express
+//server sættes til at kalde på express
 const port = 3000
 //vi vælger port 3000, vi kunne også vælge fx 5000
 
-
-
-
-//appen skal så get localhost3000 og aktivere request og response. Den vil så sende Hello World og antal
-//gange brugeren har klikket
-//JWT, måde at autorisere. hvis jeg ikke sender en JWT, får jeg "forbidden (403)" måde hvorpå servere kan genkende brugere, der tilgår api'et. 
-//laver en token, jwt.sign, og sender data med (user) i jwt'en. my secret=lang kode i tutorial
-//my secret=signatur på token.
-//options: expires dvs. når jeg får token ind, dør den, den vil sige no access
-//man sender så token til postman (klienten)
-//når man rammer protected route, går den igennem insure token.
-//token skal have et ord foran "bærer", header token sættes til at være lig med request token. 
-//"next" siger at maskinen skal gå til næste step
-//funktion får to parametre (error,data =user 0) 
 
 const harduser = require('./harduser.js')
 //indeholder array så det kan testes i postman
@@ -46,20 +32,23 @@ Server.get('/Interest1', (req, res) => {
   res.send(hardinterest[0]);
 })
 
-//User endpoint
+//User endpoint: besked, der sendes, når man opretter en bruger
 Server.post('/user/create', function (req, res){
   res.json({msg: "User created"})
 })
-//User endpoint
+//User endpoint: besked, der sendes, når brugeren sletter sin profil
 Server.delete('/user/delete', function (req, res) {
   res.json({msg: "User deleted"})
 })
-//Interest endpoint
+/*
+Interest endpoint: besked, der sendes, når brugeren opretter en beskrivelse af sine interesser, 
+såsom hobbier, beskæftigelse seksuelle preferencer 
+*/
 Server.post('/interest/create', function (req, res) {
   res.json({msg: "Interest registered"})
 }
 )
-//Match endpoint
+//Match endpoint: besked, der sendes, når brugeren sletter sine nuværende matches
 Server.delete('/match/delete', function (req, res)  {
   res.json({msg: "Match deleted"})
 })
@@ -74,7 +63,15 @@ function loginController(req, res) {
         token: token
     })
 }
-
+//JWT, måde at autorisere. hvis jeg ikke sender en JWT, får jeg "forbidden (403)" måde hvorpå servere kan genkende brugere, der tilgår api'et. 
+//laver en token, jwt.sign, og sender data med (user) i jwt'en. my secret=lang kode i tutorial
+//my secret=signatur på token.
+//options: expires dvs. når jeg får token ind, dør den, den vil sige no access
+//man sender så token til postman (klienten)
+//når man rammer protected route, går den igennem insure token.
+//token skal have et ord foran "bærer", header token sættes til at være lig med request token. 
+//"next" siger at maskinen skal gå til næste step
+//funktion får to parametre (error,data =user 0) 
 module.exports = loginController
 
 Server.listen(port, () => {
--- image.js
+++ image.js
@@ -2,11 +2,6 @@ class Image{
     constructor (Pixels, FileType) {
         this.Pixels=Pixels;
         this.FileType=FileType;
-
-        let image1 = new Image ('500px', 'PNG')
-        let image2 = new Image ('500px', 'JPG')
-        
-        ImageArray = [image1, image2];
     }
 }
 
