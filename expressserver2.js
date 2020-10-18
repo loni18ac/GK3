b96bb732c0f4ec5b3cda9db6f934bf2765783c6f
Louise <loni18ac@student.cbs.dk>
2020-10-18 12:52:17 +0200

    kk

--- expressserver2.js
+++ expressserver2.js
@@ -55,23 +55,22 @@ Server.delete('/match/delete', function (req, res)  {
 function loginController(req, res) {
     //Normalt vil man kigge om password og brugernavn stemmer, men det springer vi over
     //normalt vil man gemme secret key et andet sted. 
-    //Her laves en token, som dør om en time 
+    //Her laves en token, som dør om en time (no access)
     const token = jwt.sign({user}, 'my_secret', { expiresIn: '1h' })
     res.json({
         token: token
     })
 }
-//JWT, måde at autorisere. hvis jeg ikke sender en JWT, får jeg "forbidden (403)" måde hvorpå servere kan genkende brugere, der tilgår api'et. 
-//laver en token, jwt.sign, og sender data med (user) i jwt'en. my secret=lang kode i tutorial
-//my secret=signatur på token.
-//options: expires dvs. når jeg får token ind, dør den, den vil sige no access
-//man sender så token til postman (klienten)
-//når man rammer protected route, går den igennem insure token.
-//token skal have et ord foran "bærer", header token sættes til at være lig med request token. 
-//"next" siger at maskinen skal gå til næste step
-//funktion får to parametre (error,data =user 0) 
+
 module.exports = loginController
 
 Server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+/*
+JWT: måde at autorisere brugere på, altså så servere kan genkende brugere, der tilgår api'et. 
+Hvis jeg ikke sender en JWT, får jeg "forbidden (403)".
+My secret=signatur på token.
+Options: expires dvs. når jeg får token ind, dør den, den vil sige no access
+Man sender token til postman (klienten). Når man rammer protected route, går den igennem ensure token.
+*/
